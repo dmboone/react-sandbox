e6e7ea5d9f972f2c2f1e001c472097f2a1827274
Destiny Boone <destaj.db@gmail.com>
2023-01-31 22:01:04 -0500

    building a first custom component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,7 @@
+// a component in React is just a special kind of javascript function
+
+function ExpenseItem(){
+    return <h2>Expense item!</h2>
+}
+
+export default ExpenseItem; // must always export your component!
