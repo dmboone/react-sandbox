35561ddca53930ef8da05f1c1e6c63ded671f89e
Destiny Boone <destaj.db@gmail.com>
2023-02-08 13:37:40 -0500

    outputting dynamic data and working with expressions

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,15 +2,19 @@ import './ExpenseItem.css';
 
 // a component in React is just a special kind of javascript function
 function ExpenseItem() {
-  return (
-    <div className='expense-item'>
-      <div>March 28th 2021</div>
-      <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$294.67</div>
-      </div>
-    </div>
-  );
+    const expenseDate =  new Date(2021, 2, 28);
+    const expenseTitle =  'Car Insurance';
+    const expenseAmount =  294.67;
+
+    return (
+        <div className='expense-item'>
+        <div>{expenseDate.toISOString()}</div>
+        <div className='expense-item__description'>
+            <h2>{expenseTitle}</h2>
+            <div className='expense-item__price'>${expenseAmount}</div>
+        </div>
+        </div>
+    );
 }
 
 export default ExpenseItem; // must always export your component!
