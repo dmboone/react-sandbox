a389f499125627c43adec46c4c69f2fcc036289a
Destiny Boone <destaj.db@gmail.com>
2023-02-09 15:09:18 -0500

    listening to events and working with event handlers

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,9 @@ import Card from '../UI/Card';
 
 // a component in React is just a special kind of javascript function
 function ExpenseItem(props) {
+    const clickHandler = () => {
+        console.log('clicked!!!!')
+    }
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/> {/* since no text between the tags you can also write components like this! */}
@@ -11,6 +14,7 @@ function ExpenseItem(props) {
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
