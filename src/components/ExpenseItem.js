eed929cc011123567a37aa447c0c2e40fb0d2158
Destiny Boone <destaj.db@gmail.com>
2023-02-09 12:25:16 -0500

    composition, children props, and wrapper components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
+import Card from './Card';
 
 // a component in React is just a special kind of javascript function
 function ExpenseItem(props) {
     return (
-        <div className='expense-item'>
-        <ExpenseDate date={props.date}/> {/* since no text between the tags you can also write components like this! */}
-        <div className='expense-item__description'>
-            <h2>{props.title}</h2>
-            <div className='expense-item__price'>${props.amount}</div>
-        </div>
-        </div>
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date}/> {/* since no text between the tags you can also write components like this! */}
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
+        </Card>
     );
 }
 
