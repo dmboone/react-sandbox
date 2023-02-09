f0eb8d8bd9b11f612dbc1797a48ec03a492a5b15
Destiny Boone <destaj.db@gmail.com>
2023-02-09 15:26:55 -0500

    working with state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,22 @@
+import {useState} from 'react';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 
 // a component in React is just a special kind of javascript function
 function ExpenseItem(props) {
+    const [title, setTitle] = useState(props.title); // useState will return the current state value and the function to update the value
+
     const clickHandler = () => {
-        console.log('clicked!!!!')
-    }
+        setTitle('Updated!'); // assigns new value but also reexcutes the component function to update with the change
+        console.log(title);
+    };
+
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/> {/* since no text between the tags you can also write components like this! */}
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
