2ffc464bde87f4129922b9aee2133fa32b7b4126
Destiny Boone <destaj.db@gmail.com>
2023-02-13 13:20:51 -0500

    using one state instead, and updating state that depends on the previous state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,20 +2,60 @@ import './ExpenseForm.css';
 import { useState } from 'react';
 
 const ExpenseForm = () => {
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDate, setEnteredDate] = useState('');
+    // one solution for tracking state - separate slices
+    // const [enteredTitle, setEnteredTitle] = useState('');
+    // const [enteredAmount, setEnteredAmount] = useState('');
+    // const [enteredDate, setEnteredDate] = useState('');
 
-    const titleChangeHandler = (event) => { // event is a default value we grab
-        setEnteredTitle(event.target.value);
+    // const titleChangeHandler = (event) => { // event is a default value we grab
+    //     setEnteredTitle(event.target.value);
+    // };
+
+    // const amountChangeHandler = (event) => {
+    //     setEnteredAmount(event.target.value);
+    // };
+
+    // const dateChangeHandler = (event) => {
+    //     setEnteredDate(event.target.value);
+    // };
+
+    // another solution for tracking state - one object with all slices combined
+    const [userInput, setUserInput] = useState({
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: ''
+    });
+
+    const titleChangeHandler = (event) => {
+        // setUserInput({ // but DON'T do it like this!
+        //     ...userInput,
+        //     enteredTitle: event.target.value
+        // });
+        
+        setUserInput((prevState) => { // do it like THIS! grabbing prev state will ensure you are grabbing most up to date state;
+            return {                    // important to do it this way if you choose to update state based on previous state
+                ...prevState,   
+                enteredTitle: event.target.value
+            };
+        });
     };
 
     const amountChangeHandler = (event) => {
-        setEnteredAmount(event.target.value);
+        setUserInput((prevState) => {
+            return {
+                ...prevState,
+                enteredAmout: event.target.value
+            }
+        });
     };
 
     const dateChangeHandler = (event) => {
-        setEnteredDate(event.target.value);
+        setUserInput((prevState) => {
+            return {
+                ...prevState,
+                enteredDate: event.target.value
+            }
+        });
     };
 
     return (
