18c9aba8db39599c3d0a7ed361038956df8a3125
Destiny Boone <destaj.db@gmail.com>
2023-02-13 12:49:20 -0500

    adding form inputs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Expenses from './components/./Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => { // can also use arrow function like this instead of function App(){}
   
   return (
     <div>
       <h2>Let's get started!</h2>
+      <NewExpense></NewExpense>
       <Expenses></Expenses>
     </div>
   );
