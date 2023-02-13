a8da10d0b3b83284375279fc2243717ad74b0345
Destiny Boone <destaj.db@gmail.com>
2023-02-13 17:35:17 -0500

    outputting conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,23 +15,60 @@ function Expenses(props){
         return expense.date.getFullYear().toString() === filteredYear;
       });
 
+      {/* changes what to display based on a conditional and keeps the logic out of the jsx snippet that we return */}
+      let expensesContent = <p>No expenses found.</p>;
+      
+      if(filteredExpenses.length > 0){
+        {/* dynamically adding our expense item elements based on the amount of data in our array */}
+        {/* we use the map operator for this as seen below, which allows us to perform some action on each piece of data in our array */}
+        {/* always include a key prop that holds a unique identifier when mapping out lists of items so that React can differentiate items in the array */}
+        expensesContent = filteredExpenses.map(expense => (
+          <ExpenseItem
+          key = {expense.id}
+          title = {expense.title}
+          amount = {expense.amount}
+          date = {expense.date}
+          >
+          </ExpenseItem>
+        ));
+      }
+
       return(
         <div>
           <Card className='expenses'>
               <ExpensesFilter selectedYear={filteredYear} onChangeFilter={filterChangeHandler}/>
+              {expensesContent}
 
-              {/* dynamically adding our expense item elements based on the amount of data in our array */}
-              {/* we use the map operator for this, which allows us to perform some action on each piece of data in our array */}
-              {/* always include a key prop that holds a unique identifier when mapping out lists of items so that React can differentiate items in the array */}
-              {filteredExpenses.map(expense => (
-                <ExpenseItem
-                key = {expense.id}
-                title = {expense.title}
-                amount = {expense.amount}
-                date = {expense.date}
-                >
-                </ExpenseItem>
-              ))}
+              {/* if we wanted to put the conditional content inside the div */}
+              {/* this would be the cleanest way to write */}
+              {/* {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+              {filteredExpenses.length > 0 &&
+                filteredExpenses.map(expense => (
+                  <ExpenseItem
+                  key = {expense.id}
+                  title = {expense.title}
+                  amount = {expense.amount}
+                  date = {expense.date}
+                  >
+                  </ExpenseItem>
+                ))
+              } */}
+
+              {/* could also write the conditional like this with a normal ternary operator, just a little harder to read */}
+              {/* {filteredExpenses.length === 0 ? 
+              (<p>No expenses found.</p>) 
+              : 
+              (filteredExpenses.map(expense => (
+                  <ExpenseItem
+                  key = {expense.id}
+                  title = {expense.title}
+                  amount = {expense.amount}
+                  date = {expense.date}
+                  >
+                  </ExpenseItem>
+                ))
+              )
+              } */}
 
               {/* this code below was the old static way which couldn't update on it's own if we added something to the items array */}
               {/* <ExpenseItem 
