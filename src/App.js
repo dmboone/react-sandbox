05e687cd4bb9173233658bbae52d401251184f16
Destiny Boone <destaj.db@gmail.com>
2023-02-13 15:35:42 -0500

    moved expenses data from expenses.js to app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,33 @@ import Expenses from './components/./Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => { // can also use arrow function like this instead of function App(){}
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { 
+      id: 'e2', 
+      title: 'New TV', 
+      amount: 799.49, 
+      date: new Date(2021, 2, 12) 
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+  
   const addExpenseHandler = expense => {
     console.log('In App.js');
     console.log(expense);
@@ -11,7 +38,7 @@ const App = () => { // can also use arrow function like this instead of function
     <div>
       <h2>Let's get started!</h2>
       <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
-      <Expenses></Expenses>
+      <Expenses items={expenses}></Expenses>
     </div>
   );
 }
